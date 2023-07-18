ee7c94ccbb2ec1b738ba6064264df12c6676f4b3
mohammad <nasiri.mohammadreza92@yahoo.com>
2023-07-18 14:04:26 -0700

    create counter whit react

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,39 @@
 import React from 'react';
+import { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import pic1 from "./assets/1.png"
+import { styled } from 'styled-components';
+import styleee from './assets/css/style.module.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-const x = 5
-const arr = ["Html & css","javascript","react","jquery","bootstrap"]
-const arr2 = arr.map(myfun)
-function myfun(e , i) {
-  return <li key={i} style={{color : "red" , fontSize : 45}}>i am proficient in{e}</li>
+const Btn = styled.button`
+    margin : 40px;
+    padding : 30px
+  `
+
+  
+
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      myNumber : 5
+    }
+  }
+  render() {
+    return (
+      <>
+        <div className={styleee.box}>counter value is <span className={styleee.box2}>{this.state.myNumber}</span></div>
+        <Btn onClick={()=> this.setState({myNumber : this.state.myNumber +1})}>add1</Btn>
+        <Btn onClick={()=> this.setState({myNumber : this.state.myNumber -1})}>minus1</Btn>
+        <br />
+        <Btn onClick={()=> this.setState({myNumber : this.state.myNumber +5})}>add5</Btn>
+        <Btn onClick={()=> this.setState({myNumber : this.state.myNumber -5})}>minus5</Btn>
+      </>
+    )
+  }
 }
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
- <div>
-  <h1 style={{fontSize : 45}}>Hello world my name is mohammad nasiri</h1>
-  <img style={{width : 100 , height : 100 , cursor : "pointer"}} src = {pic1} alt="" />
-  <ul>{arr2}</ul>
- </div>
+  <Counter />
 );
 
